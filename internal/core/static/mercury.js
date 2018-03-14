b7223964fc40f10eded266daa49f022ac0dc6d5c
Ronald Doorn <rdoorn@schubergphilis.com>
2018-03-14 11:28:22 +0100

    solve style conflics

--- internal/core/static/mercury.js
+++ internal/core/static/mercury.js
@@ -1,8 +1,8 @@
 
 function errorHandler(message) {
-  $('.errormsg').stop().text(message).slideDown().delay(4000).fadeOut(500)
+  $(".errormsg").stop().text(message).slideDown().delay(4000).fadeOut(500);
 }
 
 function noticeHandler(message) {
-  $('.noticemsg').stop().text(message).slideDown().delay(4000).fadeOut(500)
+  $(".noticemsg").stop().text(message).slideDown().delay(4000).fadeOut(500);
 }
