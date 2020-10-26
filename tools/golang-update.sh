c1282dabce42c163716c4d5fbb275f1b6020eb7b
rdoorn <rdoorn@schubergphilis.com>
2020-10-26 09:26:25 +0100

    debugging (#136)

--- tools/golang-update.sh
+++ tools/golang-update.sh
@@ -1,11 +1,18 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 #PREVIOUS_VERSION=$(git describe --tags --always | cut -f1 -d-)
 #PREVIOUS_VERSION=$(git tag --sort=committerdate | tail -1)
+echo "getting latest versions:"
+git tag --sort=committerdate | sort -n -t. -k1 -k2 -k3
+
 PREVIOUS_VERSION=$(git tag --sort=committerdate | sort -n -t. -k1 -k2 -k3 | tail -1)
+echo "latest git tag: ${PREVIOUS_VERSION}"
+echo "getting latest golang.version @ https://github.com/schubergphilis/mercury/releases/download/${PREVIOUS_VERSION}/golang.version""
 RESULT=$(curl -L https://github.com/schubergphilis/mercury/releases/download/${PREVIOUS_VERSION}/golang.version -o golang.version -w "%{http_code}")
 PREVIOUS_GOLANG_VERSION=$(cat golang.version)
+echo "latest golang version: ${PREVIOUS_GOLANG_VERSION}"
 CURRENT_GOLANG_VERSION=$(go version)
+echo "image golang version: ${PREVIOUS_GOLANG_VERSION}"
 
 if [ "${RESULT}" != "200" ]; then
     echo "get golang version returned status: ${RESULT}"
